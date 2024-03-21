00d8f2299550cce81c05b47f39cf376568c94510
kittyyakki <nada9303@naver.com>
2024-03-21 17:51:23 +0900

    이어서끝

--- 01-lecture-source/chap02-operater-lecture-source/src/com/ohgiraffers/section05/logical/Application2.java
+++ 01-lecture-source/chap02-operater-lecture-source/src/com/ohgiraffers/section05/logical/Application2.java
@@ -21,5 +21,32 @@ public class Application2 {
         int num2 = 166;
         System.out.println("1부터 100 사이인지 확인 : " + (num2 >= 1 && num2 <= 100));
 
+        /* 2. 영어의 대문자인지 확인
+        *  영어 대문자냐? --> 문자변수>='A' && 문자변수<='Z' >=*/
+
+        char ch = 'G';
+        System.out.println("영어의 대문자인지 확인 : " + (ch >= 'A' && ch <= 'Z')); //아스키코드로 내부적으로 변환해서 구분해서 반환.
+
+        char ch1 = 'g';
+        System.out.println("영어의 대문자인지 확인 : " + (ch1 >= 'A' && ch1 <= 'Z'));
+
+
+        /* 3. 대소문자와 상관없이 영문자 y인지 확인 */
+        /* 문자 변수 == 'y' || 문자변수 == 'Y' */
+
+        char ch2 = 'y';
+        System.out.println("영문자 y인지 확인" + (ch2 =='y' || ch2 == 'Y'));
+
+        char ch3 = 'Y';
+        System.out.println("영문자 Y인지 확인" + (ch3 =='y' || ch3 == 'Y'));
+
+        /* 4. 영문자 인지 확인*/
+        /* A : 65, Z : 90, a : 97, z : 122 (중간의 91~96까지는 영문자가 아님)*/
+
+        //논리적으로 먼저 실행하는 연산을 묶어주는 갓이 코드이해에 더 쉽다.and 연산자는 먼저 실행되기때무네 묶을 필요 X
+
+        char ch4 = 'F';
+        System.out.println("영문자인지 확인 : "+ ((ch4>='A' && ch4<='Z')||(ch4>='a' && ch4<='z')));
+        System.out.println("영문자인지 확인 : "+ ((ch4>=65 && ch4<=90)||(ch4>=97 && ch4<=122)));
     }
 }
