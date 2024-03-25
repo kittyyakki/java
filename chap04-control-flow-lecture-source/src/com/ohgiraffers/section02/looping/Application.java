501fb81be80c8b0f1da5152eae49a4480d90f467
kittyyakki <nada9303@naver.com>
2024-03-25 16:28:07 +0900

    for 반복문

---
+++ chap04-control-flow-lecture-source/src/com/ohgiraffers/section02/looping/Application.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section02.looping;
+
+import com.ohgiraffers.section01.conditional.A_if;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        A_for a = new A_for();
+        //a.testSimpleForStatement();
+        //a.testForExample1();
+        //a.testForExample2();
+        a.testForExample3();
+    }
+}
