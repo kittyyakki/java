223bb9248be2b56ff2224326a41498e887ae261b
kittyyakki <nada9303@naver.com>
2024-04-08 12:13:33 +0900

    FileWriter

---
+++ chap14-io-lecture-source/src/com/ohgiraffers/section02/stream/Application4.java
@@ -0,0 +1,52 @@
+package com.ohgiraffers.section02.stream;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Application4 {
+    public static void main(String[] args) {
+
+        /*
+        * FileWriter
+        * 프로그램의 데이터 파일로 내보내기 위한 용도의 스트림이다
+        * 1글자 단위로 데이터를 처리한다.
+        *
+        * */
+
+        FileWriter fw = null; //먼저 null 초기화
+
+        try {
+            fw = new FileWriter("src/com/ohgiraffers/section02/stream/testWriter.txt");
+
+            fw.write(97);
+
+            fw.write('A');
+
+            fw.write(10);
+
+            fw.write(new char[]{'a','p','p','l','e'});
+
+            fw.write(10);
+
+            fw.write("아름다운 태한민국");
+
+
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (fw != null) {
+                try {
+                    fw.close();
+                } catch (IOException e) {
+                   e.printStackTrace();
+                }
+            }
+        }
+    }
+}
